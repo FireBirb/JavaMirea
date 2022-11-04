e0893223019444ac86a92c3e385f9b1d1ca4829d
FireBirb <littlefirebird1@gmail.com>
2022-11-04 21:40:36 +0300

    Finished task 13

--- ru/mirea/task13/taskStep6/Test.java
+++ ru/mirea/task13/taskStep6/Test.java
@@ -1,51 +1,80 @@
 package ru.mirea.task13.taskStep6;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
-        //...
-        Scanner scanner = new Scanner(System.in);
-        //BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String fileName = scanner.nextLine();
-        BufferedReader lineReader = new BufferedReader(new InputStreamReader
-                (new FileInputStream(fileName), StandardCharsets.UTF_8));
-        String line;
-        StringBuilder builder = new StringBuilder();
-        while ((line = lineReader.readLine()) != null) {
-            builder.append(line);
-            builder.append(" ");
+    public boolean getLine(int I, String... words){
+        //System.out.print(I+" - ");
+        //System.out.println(Arrays.toString(words));
+        //boolean endd=false;
+        boolean f=false;
+        if (I==0){
+            for(int i=0;i<words.length;i++){
+                String[] s = words.clone();
+                String temp=s[i];
+                s[i]=s[0];
+                s[0]=temp;
+                f=getLine(1,s);
+                if(f){
+                    return true;
+                }
+                /* if(f || endd){
+                    endd=true;
+                }*/
+            }
+            return false; //endd
+
         }
-        line = builder.toString().trim();    //line
-        StringBuilder result = getLine(line);
-        System.out.println(result.toString());
-    }
-    public static StringBuilder getLine(String... words) {
-        ArrayList<String> list = new ArrayList<>();
-        StringTokenizer st = new StringTokenizer(words[0], " ");
-        while (st.hasMoreTokens()) list.add(st.nextToken());
-        char next = 'А';
-        StringBuilder sb = new StringBuilder();
-        boolean haveMore;
-        while (!list.isEmpty()) {
-            haveMore = false;
-            for (String s : list) {
-                char ch = s.toUpperCase().charAt(0);
-                if (ch == next) {
-                    sb.append(s);
-                    sb.append(" ");
-                    System.out.print(s + " ");
-                    list.remove(s);
-                    next = s.toUpperCase().charAt(s.length() - 1);
-                    haveMore = true;
-                    break;
+        else {
+            for (int i=I;i<words.length;i++) {
+                if (words[i].toLowerCase().charAt(0)==words[I-1].toLowerCase().charAt(words[I-1].length()-1)) {
+                    String[] s = words.clone();
+                    String temp=s[i];
+                    s[i]=s[I];
+                    s[I]=temp;
+                    if(I == words.length-1){
+                        System.out.println(Arrays.toString(s));
+                        return true;
+                    }
+                    else {
+                        f=getLine(I+1,s);
+                        if(f){
+                            return true;
+                        }
+                        /*
+                        if(f||endd){
+                            endd=true;
+                        }
+                        */
+                    }
                 }
             }
-            if (!haveMore) break;
+            return false;//endd
         }
-        sb.deleteCharAt(sb.length() - 1);
-        return sb;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+        //C:\Users\littl\IdeaProjects\Mirea_Java\src\ru\mirea\task13\taskStep6\Words.txt
+        Path file = Paths.get(path);
+        List<String> lines;
+        try {
+            lines=Files.readAllLines(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        ArrayList<String> words = new ArrayList<>();
+        for (String line : lines) {
+            words.addAll(Arrays.asList(line.split(" ")));
+        }
+        if (!new Test().getLine(0, words.toArray(new String[0]))) {
+            System.out.println("Impossible");
+        };
     }
 }
