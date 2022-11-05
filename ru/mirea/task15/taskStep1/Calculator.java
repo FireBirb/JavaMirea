98bd47913473596b53a2c2b02279013ec520c108
FireBirb <littlefirebird1@gmail.com>
2022-11-05 22:45:59 +0300

    Finished task15 part 1

--- ru/mirea/task15/taskStep1/Calculator.java
+++ ru/mirea/task15/taskStep1/Calculator.java
@@ -11,61 +11,124 @@ import java.awt.event.*;
 //чисел, которые вводятся с клавиатуры.
 public class Calculator {
     JFrame frame = new JFrame("Calculator");
-    JButton add = new JButton("+");
-    JButton sub = new JButton("-");
+    JButton plus = new JButton("+");
+    JButton minus = new JButton("-");
     JButton div = new JButton("/");
     JButton mult = new JButton("*");
     JLabel jl1 = new JLabel("Number 1:");
     JLabel jl2 = new JLabel("Number 2");
     JTextField num1 = new JTextField(10);
     JTextField num2 = new JTextField(10);
-    //JPanel[] pan = new JPanel[8];
     public Calculator() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(2, 0, 10, 300));
-        frame.setSize(500,  500);
-        //for (int i = 0; i<8; i++) {pan[i] = new JPanel(); pan[i].setLayout(new FlowLayout()); frame.add(pan[i]);}
-        frame.add(jl1);
-        frame.add(num1);
-        frame.add(jl2);
-        frame.add(num2);
-        frame.add(add);
-        frame.add(sub);
-        frame.add(div);
-        frame.add(mult);
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(0, 10, 0, 0);
+        c.ipady = 10;
+        c.weightx = 0.3;
+        c.weighty = 0.5;
+        c.gridx = 0;
+        c.gridy = 0;
+        frame.add(jl1, c);
+        c.insets = new Insets(0, 0, 0, 10);
+        c.weightx = 0.7;
+        c.gridx = 1;
+        c.gridy = 0;
+        c.gridwidth = 3;
+        frame.add(num1, c);
+        c.insets = new Insets(0, 10, 0, 0);
+        c.weightx = 0.3;
+        c.gridx = 0;
+        c.gridy = 1;
+        frame.add(jl2, c);
+        c.insets = new Insets(0, 0, 0, 10);
+        c.weightx = 0.7;
+        c.gridx = 1;
+        c.gridy = 1;
+        c.gridwidth = 3;
+        frame.add(num2, c);
+        c.insets = new Insets(0, 0, 0, 0);
+        c.ipady = 50;
+        c.weightx = 0.5;
+        c.weighty = 0;
+        c.gridx = 0;
+        c.gridy = 2;
+        c.gridwidth = 2;
+        frame.add(plus, c);
+        c.weightx = 0.5;
+        c.gridx = 1;
+        c.gridy = 2;
+        c.gridwidth = 2;
+        frame.add(minus, c);
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 3;
+        c.gridwidth = 2;
+        frame.add(div, c);
+        c.weightx = 0.5;
+        c.gridx = 1;
+        c.gridy = 3;
+        c.gridwidth = 2;
+        frame.add(mult, c);
+        plus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String s = num1.getText();
+                try {
+                    double n1 = Double.parseDouble(num1.getText().trim());
+                    double n2 = Double.parseDouble(num2.getText().trim());
+                    double res = n1+n2;
+                    JOptionPane.showMessageDialog(null, "Result = "+res,"Alert",JOptionPane.INFORMATION_MESSAGE);
+                } catch (Exception er){
+                    JOptionPane.showMessageDialog( null, "Error in Numbers !","alert" , JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        minus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String s = num1.getText();
+                try {
+                    double n1 = Double.parseDouble(num1.getText().trim());
+                    double n2 = Double.parseDouble(num2.getText().trim());
+                    double res = n1-n2;
+                    JOptionPane.showMessageDialog(null, "Result = "+res,"Alert",JOptionPane.INFORMATION_MESSAGE);
+                } catch (Exception er){
+                    JOptionPane.showMessageDialog( null, "Error in Numbers !","alert" , JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        mult.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String s = num1.getText();
+                try {
+                    double n1 = Double.parseDouble(num1.getText().trim());
+                    double n2 = Double.parseDouble(num2.getText().trim());
+                    double res = n1*n2;
+                    JOptionPane.showMessageDialog(null, "Result = "+res,"Alert",JOptionPane.INFORMATION_MESSAGE);
+                } catch (Exception er){
+                    JOptionPane.showMessageDialog( null, "Error in Numbers !","alert" , JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        div.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String s = num1.getText();
+                try {
+                    double n1 = Double.parseDouble(num1.getText().trim());
+                    double n2 = Double.parseDouble(num2.getText().trim());
+                    double res = n1/n2;
+                    JOptionPane.showMessageDialog(null, "Result = "+res,"Alert",JOptionPane.INFORMATION_MESSAGE);
+                } catch (Exception er){
+                    JOptionPane.showMessageDialog( null, "Error in Numbers !","alert" , JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        frame.setSize(new Dimension(500, 700));
         frame.setVisible(true);
-        /*MilanButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Milan++;
-                score.setText("Result: " + Milan + " X " + Madrid);
-                lastScorer.setText("Last scorer: AC Milan");
-                if (Milan == Madrid) winner.setText("Winner: DRAW");
-                else if (Milan > Madrid) winner.setText("Winner: AC Milan");
-            }
-        });
-        RealButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Madrid++;
-                score.setText("Result: " + Milan + " X " + Madrid);
-                lastScorer.setText("Last scorer: Real Madrid");
-                if (Madrid == Milan) winner.setText("Winner: DRAW");
-                else if (Madrid > Milan) winner.setText("Winner: Real Madrid");
-            }
-        });
-        setVisible(true);
-
-        setTitle("A picture");
-        Scanner scanner = new Scanner(System.in);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        System.out.println("Enter full path to the picture, including its name");
-        String path = scanner.nextLine();
-        //ImageIcon image = new ImageIcon("C:\\Users\\littl\\IdeaProjects\\Mirea_Java\\src\\ru\\mirea\\task5\\taskStep3\\cat.jpg");
-        ImageIcon image = new ImageIcon(path);
-        JLabel label = new JLabel(image);
-        add(label);
-        pack();*/
     }
 
     public static void main(String[] args) {
